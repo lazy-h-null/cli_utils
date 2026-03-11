bb97f0d2c062cf406b40b202830e476599940e1a
lazy-h-null <259433021+YooriHan@users.noreply.github.com>
2026-03-11 01:30:32 -0700

    Update cli_utils to v0.2.0 with challenges

--- cli_utils/__init__.py
+++ cli_utils/__init__.py
@@ -1,1 +1,7 @@
-from .separators import print_separator
+from .separators import (
+    print_separator, 
+    print_char_separator, 
+    print_custom_separator, 
+    print_labeled_separator, 
+    print_box
+)
--- cli_utils/separators.py
+++ cli_utils/separators.py
@@ -4,3 +4,18 @@ def print_separator():
 
 def print_char_separator(char):
     print(char * 30)
+
+def print_custom_separator(char, length):
+    if length <=0:
+        print("")
+        return
+    print(char * length)
+
+def print_labeled_separator(label, char="*", width=30):
+    print(label.center(width, char))
+
+def print_box(message, char="*"):
+    width = len(message) + 4
+    print(char * width)
+    print(char + " " + message + " " + char)
+    print(char * width)
